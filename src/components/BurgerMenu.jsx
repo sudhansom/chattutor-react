279cc1cd582fbb16f16ee616bb1ca50c6378e989
bhuwan <bkspoudel@gmail.com>
2024-05-02 21:25:43 +0200

    create a burger menu

---
+++ src/components/BurgerMenu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BurgerMenu = () => {
+  return (
+    <div className="flex flex-col gap-1.5 w-6 ">
+      <div className="h-0.5 bg-slate-950"></div>
+      <div className="h-0.5 bg-slate-950"></div>
+      <div className="h-0.5 bg-slate-950"></div>
+    </div>
+  );
+};
+
+export default BurgerMenu;
--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./BurgerMenu";
+import BurgerMenu from "./BurgerMenu";
+
 const TopNavBar = () => {
-  return <div>TopNavBar</div>;
+  return (
+    <div>
+      <BurgerMenu />
+    </div>
+  );
 };
 
 export default TopNavBar;
