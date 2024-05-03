d6f56e5f9bac82259d0b0a316bb0e6168326524d
bhuwan <bkspoudel@gmail.com>
2024-05-03 20:52:28 +0200

    fix burger hovering

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const BurgerMenu = () => {
   return (
     <div className="flex flex-col gap-1.5 w-6 hover:cursor-pointer mt-1 group hover:transition-all">
-      <div className="h-0.5 bg-slate-500 group-hover:bg-red-900"></div>
-      <div className="h-0.5 bg-slate-500 group-hover:bg-red-900"></div>
-      <div className="h-0.5 bg-slate-500 group-hover:bg-red-900"></div>
+      <div className="h-0.5 bg-slate-500 group-hover:bg-slate-900"></div>
+      <div className="h-0.5 bg-slate-500 group-hover:bg-slate-900"></div>
+      <div className="h-0.5 bg-slate-500 group-hover:bg-slate-900"></div>
     </div>
   );
 };
