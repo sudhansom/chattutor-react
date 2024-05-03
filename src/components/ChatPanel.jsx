302be8dd9086f6d3e345dc2eefd92410339ec54c
bhuwan <bkspoudel@gmail.com>
2024-05-03 12:03:07 +0200

    fix gpt image

--- src/components/ChatPanel.jsx
+++ src/components/ChatPanel.jsx
@@ -1,10 +1,20 @@
 import TextArea from "./ui/TextArea";
 import ChatTutorTitle from "./ChatTutorTitle";
+import GptImage from "../assets/images/gpt-image.png";
+
 const ChatPanel = () => {
   return (
     <div className="relative h-full border basis-1/2 text-center p-4 bg-gray-200">
       <div className="flex gap-4 justify-between px-4">
-        <span>icon</span>
+        <div>
+          <img
+            className="rounded-full"
+            src={GptImage}
+            height="40"
+            width="40"
+            alt="chat gpt image"
+          />
+        </div>
         <ChatTutorTitle />
       </div>
       <div className="w-full absolute bottom-0 left-0 rounded overflow-hidden p-2">
