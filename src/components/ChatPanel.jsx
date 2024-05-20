8f9bf8a14d17fa8bb2a89d2b22b8c6f45ef1ac9e
bhuwan <bkspoudel@gmail.com>
2024-05-20 20:58:29 +0200

    pass function as a parameter to update chat

--- src/components/ChatPanel.jsx
+++ src/components/ChatPanel.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import TextArea from "./ui/TextArea";
 import ChatTutorTitle from "./ChatTutorTitle";
 import GptImage from "../assets/images/gpt-image.png";
@@ -6,6 +7,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 
 const ChatPanel = () => {
+  const [chat, setText] = useState('');
+  function longText (text) {
+    setText(text)
+  }
   return (
     <div className="relative h-full border grow text-center p-4 bg-gray-200">
       <div className="flex gap-4 justify-between px-4">
@@ -20,8 +25,9 @@ const ChatPanel = () => {
         </div>
         <ChatTutorTitle />
       </div>
+      {chat && <p>{{chat}}</p>}
       <div className="w-full absolute bottom-0 left-0 rounded overflow-hidden p-2">
-        <TextArea />
+        <TextArea longText={longText} />
         <FontAwesomeIcon
           icon={faGear}
           className="absolute top-4 right-4 text-gray-500 hover:cursor-pointer hover:text-gray-700 transition duration-200 ease-in-out"
