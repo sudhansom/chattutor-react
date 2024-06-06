099c5d6cf385af31dd33a644bb9b85517b02cb3e
bhuwan <bkspoudel@gmail.com>
2024-06-06 07:55:57 +0200

    put image in setting for now

--- src/components/SettingPanel.jsx
+++ src/components/SettingPanel.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import settingImg from '../assets/images/setting.png'
+
 const SettingPanel = () => {
   return (
-    <div className="border h-full basis-1/4 text-center p-4 text-2xl font-bold">
-      SettingPanel
+    <div className="border h-full basis-1/4 text-center p-0 text-2xl font-bold">
+      <img className="object-cover" src={settingImg} alt="" />
     </div>
   );
 };
