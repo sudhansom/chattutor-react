55ee4834f23b197d1d94c06d16a28f358b2ff123
bhuwan <bkspoudel@gmail.com>
2024-06-09 11:58:13 +0200

    fix sup text to heading

--- src/components/WelcomeText.jsx
+++ src/components/WelcomeText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WelcomeText = () => {
   return (
       <div className="welcome-text">
-        <h2>Welcome back, TueHerlau</h2>
+        <h2>Welcome back, TueHerlau<sup><span>Teacher</span></sup></h2>
         <p>A8000 (Mikkel Semester)</p>
       </div>
   );
