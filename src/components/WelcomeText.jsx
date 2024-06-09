817e87a02ab9d1db9833718bd0acf81fe7146fc2
bhuwan <bkspoudel@gmail.com>
2024-06-09 12:18:54 +0200

    fix welcome part and the developer switch

--- src/components/WelcomeText.jsx
+++ src/components/WelcomeText.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const WelcomeText = () => {
   return (
-      <div className="welcome-text">
+      <div className="welcome-text position-relative">
         <h2>Welcome back, TueHerlau <sup><span>Teacher</span></sup></h2>
         <p>A8000 (Mikkel Semester)</p>
+        <div className="form-check form-switch position-absolute right-2 mb-neg-24">
+          <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Developer mode</label>
+        </div>
       </div>
   );
 };
